0c2461b597984493fc98740d276c8a1e48934ab6
m-diana-dev <m.diana.dev@gmail.com>
2023-04-25 17:05:13 +0300

    Ñlessons 1-9

---
+++ src/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Header = () => {
+    return (
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Home</li>
+                <li>Home</li>
+                <li>Home</li>
+            </ul>
+        </nav>
+    )
+}
---
+++ src/Technologies.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const Technologies = () => {
+    return (
+        <div>
+            <h1>Technologies</h1>
+            <ul>
+                <li>css</li>
+                <li>html</li>
+                <li>js</li>
+                <li>react</li>
+            </ul>
+        </div>
+    )
+}
+
